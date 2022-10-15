a17ea3e750390e178262a92b5114cc12760f244f
Darya <darya.novosadova@gmail.com>
2022-10-15 12:07:52 +0700

    add Seminar1 004

---
+++ Seminar1_004/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите положительное число: ");
+int number = int.Parse(Console.ReadLine());
+int count = number * -1;
+while(count <= number)
+{
+    Console.Write($"{count}, ");
+    count++;
+}
