92ed42917504c377df7cdd982fab4989c909f75d
Darya <darya.novosadova@gmail.com>
2022-10-15 12:18:24 +0700

    add Seminar1 005

---
+++ Seminar1_005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if((number < 100) || (number > 999))
+{
+    Console.WriteLine("Введите трехзначное число: ");
+}
+else
+{
+    int EndOfNumber = number%10;
+    Console.Write(EndOfNumber);
+}
