154ebc563cab4072a66d2a064af2dd677b8c16a2
Darya <darya.novosadova@gmail.com>
2022-10-15 10:16:03 +0700

    add Seminar1 001

---
+++ Seminar1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()); //Квадрат числа
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
