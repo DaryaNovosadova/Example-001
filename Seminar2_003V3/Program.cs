a00fb0a50ad8cad83c82d0f7507b2f01c41a78be
Darya <darya.novosadova@gmail.com>
2022-10-19 17:44:09 +0700

    add Seminar2 003 V3

---
+++ Seminar2_003V3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter your number");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int copy = number;
+int revers = 0;
+while(copy > 0)
+{
+    int digit = copy %10;
+    revers = revers * 10;
+    revers = revers + digit;
+    copy = copy / 10;
+}
+Console.WriteLine(revers);
+if(number == revers)
+    Console.WriteLine("Polindrome");
+else
+    Console.WriteLine("Not polindrome");
