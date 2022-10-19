3894023e61b365fea65830a4ac7e66c081b2d009
Darya <darya.novosadova@gmail.com>
2022-10-19 16:21:23 +0700

    add Seminar2 001

---
+++ Seminar2_001/Program.cs
@@ -0,0 +1,13 @@
+﻿// дано число. Проверить кратно они 7 и 23
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно 7 и 23");
+}
