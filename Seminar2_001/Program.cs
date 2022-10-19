7d1dc3e435324f95ac0e8e1ce4cf3df2738028ba
Darya <darya.novosadova@gmail.com>
2022-10-19 16:33:40 +0700

    add Seminar2 001

--- Seminar2_001/Program.cs
+++ Seminar2_001/Program.cs
@@ -1,7 +1,7 @@
 ﻿// дано число. Проверить кратно они 7 и 23
 
 Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine()!);
+int number = int.Parse(Console.ReadLine() ?? "0");
 
 if(number % 7 == 0 && number % 23 == 0)
 {
