f5152309b8518f6211cd3ed8a54b116475780296
Darya <darya.novosadova@gmail.com>
2022-10-19 17:02:55 +0700

    add Seminar2 002V2

---
+++ Seinar2_002V2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Определения четверти плоскости");
+
+Console.Write("Ввидите координату x: ");
+int X = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Ввидите координату y: ");
+int Y = int.Parse(Console.ReadLine() ?? "0");
+
+if(X > 0 && Y > 0)
+{
+    Console.WriteLine("Первая четверть");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Вторая четверть");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Третья четверть");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Четвертая четверть");
+}
