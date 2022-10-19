4b7bae9b6586776674fa433dae539da016abe0ab
Darya <darya.novosadova@gmail.com>
2022-10-19 17:17:03 +0700

    add Seminar2 003

---
+++ Seminar2_003/Program.cs
@@ -0,0 +1,19 @@
+﻿//Программа проверяет пятизначное число на полиндром
+Console.Write("Ввидите пятизначное число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int [] array = new int[5];  //создаем массив
+int index = 4;              //исследуем элементы массива с конца
+while(index >= 0)
+{
+    array[index] = number%10;  //получаем последнюю цифру в массиве
+    number = number/10;        //переходим к следующиму числу
+    index--;                   //переходим к следующиму элементу массива
+}
+if(array[0] == array[4] && array[1] == array[3])
+{
+    Console.WriteLine("Число является полиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является полиндромом");
+}
