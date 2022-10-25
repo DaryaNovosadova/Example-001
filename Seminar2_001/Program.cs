1f2b6d4a6e63a4b3745c7535504b61c2ed1112ae
Darya <darya.novosadova@gmail.com>
2022-10-25 15:06:55 +0700

    add Example 012 Methods

--- Seminar2_001/Program.cs
+++ Seminar2_001/Program.cs
@@ -3,11 +3,11 @@
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine() ?? "0");
 
-if(number % 7 == 0 && number % 23 == 0)
+if(number % 6 == 0 && number % 12 == 0)
 {
-    Console.WriteLine($"Число {number} кратно 7 и 23");
+    Console.WriteLine($"Число {number} кратно 6 и 24");
 }
 else
 {
-    Console.WriteLine($"Число {number} не кратно 7 и 23");
+    Console.WriteLine($"Число {number} не кратно 6 и 24");
 }
